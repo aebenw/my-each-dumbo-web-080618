dbb2268075d42e8cce9390613dbed888f34585b4
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 23:15:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,9 +3,10 @@ def my_each(arr)
   if block_given?
     i = 0 
     while i < arr.length
-    yeild(new << arr[i])
+    yeild(arr[i])
     i += 1 
 end
 end
 
-my_each([1,2,3,4]) do |num|
+n.times my_each([1,2,3,4]) do |num|
+  
